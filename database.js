fa845ae93b3302c1c882bcc048f008fa22fa0aa4
kamalraj D <kamalrajdhanakodi@gmail.com>
2021-10-02 18:11:06 +0530

    initial database module

---
+++ database.js
@@ -0,0 +1,24 @@
+var pg = require("pg");
+//or native libpq bindings
+//var pg = require('pg').native
+
+var conString =
+  "postgres://pajdauwo:EnuOogaf3i5XhIOrDPHbYWtZpiSZlBol@satao.db.elephantsql.com/pajdauwo"; //Can be found in the Details page
+var client = new pg.Client(conString);
+const getTime = () =>
+  client.connect(function (err) {
+    if (err) {
+      return console.error("could not connect to postgres", err);
+    }
+    client.query('SELECT NOW() AS "theTime"', function (err, result) {
+      if (err) {
+        return console.error("error running query", err);
+      }
+      console.log(result.rows[0].theTime);
+      // >> output: 2018-08-23T14:02:57.117Z
+      client.end();
+    });
+  });
+
+//postgres://pajdauwo:EnuOogaf3i5XhIOrDPHbYWtZpiSZlBol@satao.db.elephantsql.com/pajdauwo
+module.exports = { getTime };
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const path = require("path");
+const Database = require("./database");
+
+Database.getTime();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
