ae9c98da1f1000a80c6266829d5b1572f5c970ce
kamalraj D <kamalrajdhanakodi@gmail.com>
2021-10-03 19:14:33 +0530

    added checkUser function

--- database.js
+++ database.js
@@ -3,8 +3,9 @@ var pg = require("pg");
 //var pg = require('pg').native
 
 var conString = process.env.DB_URI; //Can be found in the Details page
-var client = new pg.Client(conString);
-const getTime = () =>
+
+const getTime = () => {
+  var client = new pg.Client(conString);
   client.connect(function (err) {
     if (err) {
       return console.error("could not connect to postgres", err);
@@ -18,5 +19,47 @@ const getTime = () =>
       client.end();
     });
   });
+};
 
-module.exports = { getTime };
+const getUserDetail = (email) => {
+  var client = new pg.Client(conString);
+  client.connect(function (err) {
+    if (err) {
+      return console.error("could not connect to postgres", err);
+    }
+    client.query(
+      "SELECT * FROM user WHERE email = " + email,
+      function (err, result) {
+        if (err) {
+          return console.error("error running query", err);
+        }
+        console.log(result.rows[0].theTime);
+        // >> output: 2018-08-23T14:02:57.117Z
+        client.end();
+      }
+    );
+  });
+};
+
+const checkUser = async (email_id, password) => {
+  var client = new pg.Client(conString);
+  var validity = false;
+
+  try {
+    await client.connect();
+    const result = await client.query(
+      "SELECT email_id, password FROM users WHERE email_id = $1",
+      [email_id]
+    );
+    //console.log("result ", result);
+    if (result.rows[0]["password"].toString() === password.toString())
+      validity = true;
+    //console.log(result.rows[0]["password"], password);
+  } catch (e) {
+    console.log(e.stack);
+  }
+  client.end();
+  return validity;
+};
+
+module.exports = { getTime, checkUser };
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const Database = require("./database");
+const bodyParser = require("body-parser");
 
 Database.getTime();
 
@@ -11,9 +12,19 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // api's
 // database api functions
-app.get("/database/sign_in");
-app.get("/database/sign_up");
-app.get("/database/get_user_detail");
+app.get("/database/sign_in", async (req, res) => {
+  //console.log(req.body);
+  const validity = await Database.checkUser(
+    req.body["email_id"],
+    req.body["password"]
+  );
+  //console.log(validity);
+  if (validity) res.send("signed in");
+  else res.send("not signed in");
+});
+app.get("/database/sign_up", (req, res) => res.send("sign_up"));
+app.get("/database/get_user_detail", (req, res) => res.send("get user detail"));
